41d94c11df610663918cb9ea4642ac075765e882
Sergey Poskachey <seregynp@gmail.com>
2019-03-21 17:51:50 +0400

    15-refactor-1.0.0: Reset

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-
-export { combineDiReducers } from './combineDiReducers';
-export { makePlainReducer } from './makePlainReducer';
-export { makeDiReducer } from './makeDiReducer';
-export { DiChanges } from './ExReducerDependenciesChanges';
--- src/makePlainReducer.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import type {Reducer} from './types';
-
-export const makePlainReducer = <S, A>(
-  initialState: S,
-  reducer: Reducer<S, A>,
-): Reducer<S, A> =>
-  (state, action) => (state === undefined ? initialState : reducer(state, action));
