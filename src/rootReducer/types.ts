13b66b1ff61905d77535fd337625aac8485077ea
Sergey Poskachey <sepo@one.com>
2019-04-28 11:32:20 +0400

    15-refactor-1.0.0: combineReducers - reset

--- src/rootReducer/types.ts
+++
@@ -1,6 +0,0 @@
-import { Action, AnyAction, MapS, Reducer } from '../types'; // eslint-disable-line no-unused-vars
-import { DiReducer } from '../diReducer/types'; // eslint-disable-line no-unused-vars
-
-export type RootReducersMap<S, A extends Action = AnyAction> = {
-  [K in keyof S]: Reducer<S[K], A> | DiReducer<S[K], A>
-};
