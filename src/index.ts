fe002dde993590c861b3855886a452933b466a53
Sergey Poskachey <seregynp@gmail.com>
2019-06-18 22:40:16 +0400

    Prepare 1.0.0-alpha+1 publish

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { combineReducers } from './combineReducers/combineReducers';
+export { diReducer } from './diReducer/diReducer';
+export { diMemoReducer } from './diReducer/diMemoReducer';
+export { DiSelector } from './diSelector/DiSelector';
+export { initReducer } from './initReducer/initReducer';
