2746f7624536c426ce4f8bcdc57437ddafd478aa
Sergey Poskachey <sepo@one.com>
2019-07-03 11:40:10 +0400

    Update testAppReducer

--- src/testAppReducer/testAppReducer.ts
+++ src/testAppReducer/testAppReducer.ts
@@ -1,14 +1,13 @@
 import { path as rPath, assocPath as rAssocPath } from 'ramda';
 import { AppSel } from '../appSelector/types'; // eslint-disable-line no-unused-vars
 import { MapS, Reducer } from '../types'; // eslint-disable-line no-unused-vars
-import { dummyAction } from '../../tests/actions';
 import { ReduxDiError } from '../utils/ReduxDiError';
 import { toArrPath } from '../utils/path'; // eslint-disable-line no-unused-vars
 
 export const testAppReducer = <S, R>(appReducer: Reducer<S>, selector: AppSel<S, R>): Reducer<R> => {
   const
     selectorPath = selector._path, // eslint-disable-line no-underscore-dangle
-    initialAppState = appReducer(undefined, dummyAction());
+    initialAppState = appReducer(undefined, { type: '@@INIT' });
 
   if (rPath(selectorPath, initialAppState) === undefined) {
     throw new ReduxDiError(`Non existent app selector path "${selectorPath.join('.')}" given to testAppReducer`);
