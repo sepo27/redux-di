f4ae93bf26f389d2e4e0f0dd53bd71f0cf98a3c3
Sergey Poskachey <sepo@one.com>
2019-07-03 11:27:29 +0400

    Export testAppReducer

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { diReducer } from './diReducer/diReducer';
 export { diMemoReducer } from './diReducer/diMemoReducer';
 export { DiSelector } from './diSelector/DiSelector';
 export { initReducer } from './initReducer/initReducer';
+export { testAppReducer } from './testAppReducer/testAppReducer';
